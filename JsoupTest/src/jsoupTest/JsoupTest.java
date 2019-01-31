2ed6102516fa8bc66b548083e49039e715664239
rahmanole <rahmanole.bd@gmail.com>
2019-01-31 19:01:59 +0600

    up

--- JsoupTest/src/jsoupTest/JsoupTest.java
+++ JsoupTest/src/jsoupTest/JsoupTest.java
@@ -1,9 +1,10 @@
 package jsoupTest;
 
 import java.io.IOException;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.*;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,50 +12,75 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class JsoupTest {
+	static TreeSet<String> postsIdSet1 = new TreeSet<>();
+	static TreeSet<String> lastpostsId = new TreeSet<>();
+	
 	public static void main(String[] args) throws IOException {
 		forProthomAlo();
 		// forKalerKontho();
 		// forJugantor();
+		
+//		System.out.println(postsId.size());
+//		for (String s : postsId) {
+//			System.out.println(s);
+//		}
+		
+		extractContent("https://www.prothomalo.com/bangladesh/article/");
 	}
 
 	// For prothom alo
 	static void forProthomAlo() throws IOException {
-		TreeSet<String> linksSet = new TreeSet<>();
+		String categoryUrl = "https://www.prothomalo.com/bangladesh/article/";
+		TreeSet<String> postsIdSet2 = new TreeSet<>();
 		Document document = Jsoup
-				.connect("https://www.prothomalo.com/bangladesh/article/")
+				.connect(categoryUrl)
 				.get();
 		Element body = document.body();
-		Elements posts = document
-				.getElementsByClass("each");
-		Elements postsUrl = posts.select("a[href]");
+		Elements posts = body.getElementsByClass("each");
+		Elements post = posts.select("a[href]");
 
-
-		int count = 0;
-		for (Element link : postsUrl) {
+		for (Element link : post) {
 			String url = link.attr("href");
-			Pattern pattern = Pattern.compile("[0-9]+");
+			Pattern pattern = Pattern.compile("[0-9]{7}");
 			Matcher matcher = pattern.matcher(url);
 			if (matcher.find()) {
 				if (matcher.group().length() != 7) {
 					break;
 				} else {
-					linksSet.add(matcher.group());
+					postsIdSet2.add(matcher.group());
 				}
 			}
 		}
-
-		// System.out.println(linksSet.size());
-		for (String s : linksSet) {
-			System.out.println(s);
+		if( postsIdSet2.containsAll(postsIdSet1)) {
+			
 		}
-
-		Elements postImages = document.select("img[src]");
-		for (Element link : postImages) {
-			String imgUrl = link.attr("src");
-			System.out.println(imgUrl);
-		}
-
+		lastpostsId.add("1576956");
+		lastpostsId.add("1576955");
+		
+		
 	}
+	
+	
+	public static void extractContent(String postUrl) throws IOException{
+		for(String s:lastpostsId) {
+			
+			Document document = Jsoup
+					.connect(postUrl+s)
+					.get();
+			
+			Element body = document.body();
+			Elements heading = body.getElementsByTag("h1");
+			System.out.println(heading.text());
+			Elements posts = body.getElementsByTag("article");
+			
+			for (Element link : posts) {
+				System.out.println(link.text());
+			}
+		}
+	}
+
+
+	
 
 	// For kalerkontho
 	static void forKalerKontho() throws IOException {
